c6ba90f18546c9fb61cafb5350b577027d57d29a
justinharding <justin.harding@starboard.nz>
2024-08-19 10:52:26 +1200

    put command line back at bottom; add vim-just syntax

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,14 @@
+return {
+  -- Configure LazyVim to load gruvbox
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {
+        enabled = false,
+      },
+      messages = {
+        enabled = false,
+      },
+    },
+  },
+}
