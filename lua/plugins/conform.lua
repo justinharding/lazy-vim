561fb342bef9712cc90e2fe8c8fdc39474be0ee2
justinharding <justin.harding@starboard.nz>
2024-10-17 12:45:29 +1000

    add commented section to conform.lua showing how to use a specific copy of sqlfluff

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,6 +4,11 @@ return {
     opts = {
       formatters_by_ft = {
         sql = { "sqlfluff" },
+        -- sql = {
+        --   "sqlfluff",
+        --   args = { "--dialect", "postgres" }, -- Add any additional args
+        --   command = "/opt/homebrew/anaconda3/bin/sqlfluff", -- Path to global sqlfluff
+        -- },
       },
     },
   },
