8c44f6c39bd294fbdb6063e3e60d9dc93d4812b3
justinharding <justin.harding@starboard.nz>
2024-10-23 07:36:30 +1000

    update sql formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,6 +3,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        -- sql = { "sqlfmt" },
         sql = { "sqlfluff" },
         -- sql = {
         --   "sqlfluff",
