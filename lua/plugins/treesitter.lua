f686d782ae45aa60f1ade793ca478c07b3d1b776
justinharding <justin.harding@starboard.nz>
2025-07-22 12:29:11 +1200

    used treesitter for justfile instead of plugin; add treesitter indent support; add treesitter incremental selection gsb, gse, gss

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,13 +3,24 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = { "graphql" }, -- Add other languages as needed
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "graphql", "just" },
         highlight = {
           enable = true,
           additional_vim_regex_highlighting = false,
         },
-      }
+        indent = {
+          enable = true, -- Use Treesitter for automatic indentation where possible
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gsb", -- start selection
+            node_incremental = "gse", -- expand node
+            node_decremental = "gss", -- shrink node
+          },
+        },
+      })
     end,
   },
 }
