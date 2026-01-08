582db9c53b1f23d2df87b79bb0cd4879e3a59090
Justin Harding <justin.harding@starboard.nz>
2026-01-08 22:35:52 +1300

    more changes for zellij I think

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,30 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Automatically rename Zellij tab to the current file name
+vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
+  group = vim.api.nvim_create_augroup("ZellijTabName", { clear = true }),
+  callback = function()
+    -- Only run if we are actually inside a Zellij session
+    if vim.env.ZELLIJ then
+      local file_name = vim.fn.expand("%:t")
+      if file_name ~= "" then
+        -- Use the Zellij CLI to rename the current tab
+        -- 'action' commands are very fast
+        os.execute(string.format("zellij action rename-tab '󰈚 %s'", file_name))
+      else
+        os.execute("zellij action rename-tab '󰉋 empty'")
+      end
+    end
+  end,
+})
+
+-- Reset tab name when exiting Neovim
+vim.api.nvim_create_autocmd("VimLeave", {
+  callback = function()
+    if vim.env.ZELLIJ then
+      os.execute("zellij action rename-tab 'zsh'")
+    end
+  end,
+})
--- lua/plugins/smart_split.lua
+++ lua/plugins/smart_split.lua
@@ -16,6 +16,12 @@ return {
     { '<C-j>', function() require('smart-splits').move_cursor_down() end, desc = 'Move down' },
     { '<C-k>', function() require('smart-splits').move_cursor_up() end, desc = 'Move up' },
     { '<C-l>', function() require('smart-splits').move_cursor_right() end, desc = 'Move right' },
+
+    -- Optional: Seamless Resizing (use Alt instead of Ctrl)
+    { '<A-h>', function() require('smart-splits').resize_left() end, desc = 'Resize left' },
+    { '<A-j>', function() require('smart-splits').resize_down() end, desc = 'Resize down' },
+    { '<A-k>', function() require('smart-splits').resize_up() end, desc = 'Resize up' },
+    { '<A-l>', function() require('smart-splits').resize_right() end, desc = 'Resize right' },
   },
 }
 
