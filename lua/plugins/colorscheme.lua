2ef4ceb577e0ff4e1109a52227d40370f57f78c2
Justin Harding <justin.harding@starboard.nz>
2026-03-19 10:40:52 +1300

    make background black

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,49 @@
-return {
-  -- add gruvbox
-  {
-    "catppuccin",
-    opts = {
-      transparent_background = true,
-    },
-  },
+-- return {
+--   -- add gruvbox
+--   {
+--     "catppuccin",
+--     opts = {
+--       transparent_background = true,
+--     },
+--   },
+--
+--   -- Configure LazyVim to load catppuccin
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catppuccin",
+--     },
+--   },
+-- }
 
-  -- Configure LazyVim to load catppuccin
+
+-- this it to get a black background
+return {
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- or macchiato, frappe, latte
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = false,
+        show_end_of_buffer = false,
+        color_overrides = {
+          mocha = {
+            base = "#000000",
+            mantle = "#000000",
+            crust = "#000000",
+          },
+        },
+      })
+
+      -- setup must be called before loading
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
 }
