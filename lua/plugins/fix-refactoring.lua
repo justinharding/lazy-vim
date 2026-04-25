7681bbc5e945a5ff00bf467ea42198cdf155dc06
Justin Harding <justin.harding@starboard.nz>
2026-04-25 18:07:15 +1200

    temporary patch for refactoring plugin while build is broken by developers

---
+++ lua/plugins/fix-refactoring.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    -- 22 apr 2026 - this is temooorary - refactoring plugin is broken
+    -- claude gave me this workaround-
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = { "lewis6991/async.nvim" },
+  },
+}
