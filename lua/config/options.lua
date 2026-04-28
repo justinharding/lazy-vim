638458c1dc810eb83496e4e07ac9864a9c956757
Justin Harding <justin.harding@starboard.nz>
2026-04-28 19:02:46 +1200

    use nvim internal copy/paste for normal stuff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,7 +18,13 @@ if vim.env.SSH_CONNECTION or vim.env.SSH_TTY then
   }
 end
 
-vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
+-- vim.opt.clipboard = "unnamedplus"   -- removed
+
+-- explicit bridges
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
+vim.keymap.set('n',        '<leader>Y', '"+Y', { desc = 'Yank line to system clipboard' })
+vim.keymap.set({'n', 'v'}, '<leader>p', '"+p', { desc = 'Paste from system clipboard' })
+vim.keymap.set({'n', 'v'}, '<leader>P', '"+P', { desc = 'Paste before from system clipboard' })
 
 vim.o.background = "dark"
 vim.g.lazyvim_colorscheme = "catppuccin-mocha"
